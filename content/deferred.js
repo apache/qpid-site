6125fc4053edfcddc923769454a93e202d21638d
Lorenz Quack <lquack@apache.org>
2016-06-07 13:46:10 +0100

    QPID-7294: Add project specific security page

--- content/deferred.js
+++ content/deferred.js
@@ -420,6 +420,18 @@ function _modifyCurrentReleaseLinks() {
     }
 }
 
+function _toggleDiv(toggleInfo) {
+    var div = document.getElementById(toggleInfo.divId);
+    var control = document.getElementById(toggleInfo.controlId);
+    if (div.style.display !== "none") {
+	div.style.display = "none";
+	control.innerHTML = toggleInfo.showMore;
+    } else {
+	div.style.display = "block";
+	control.innerHTML = toggleInfo.showLess;
+    }
+}
+
 _updateGlobalNavigation();
 
 _addEventListener("-menu-link", "click", _toggleMenu);
